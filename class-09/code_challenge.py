dff9c2b44fdbaa3cd5ffdeb3cc60d117d683025b
Roger T. Huba <rogerhuba@gmail.com>
2021-01-09 12:24:46 -0600

    changes to code challenge

--- class-09/code_challenge.py
+++ class-09/code_challenge.py
@@ -1,6 +1,6 @@
-def count_common_nodes(head1, head2): 
-    current1 = head1 
-    current2 = head2 
+def count_common_nodes(ll_1, ll_2): 
+    current1 = ll_1.head
+    current2 = ll_2.head 
     count = 0
   
     while current1: 
@@ -12,6 +12,6 @@ def count_common_nodes(head1, head2):
             current2 = current2.next
           
         current1 = current1.next
-        current2 = head2 
+        current2 = ll_2.head 
       
     return count 
